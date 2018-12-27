24fdca99dd18e486828ec70f044ff2ca36a6909a
DannyLau <danny1251597530@gmail.com>
2018-12-27 09:28:53 +0800

    Modify UI, and calendar

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart'
+    show CalendarCarousel;
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
- @override
-   Widget build(BuildContext context) {
-     // TODO: implement build
-     return MaterialApp(
-       theme: ThemeData(
-         primaryColor: Colors.indigo[900]
-       ),
-       title:'My Calender',
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.indigo[900]),
+      title: 'My Calender',
       home: Scaffold(
         appBar: AppBar(
-          title: Row(children: <Widget>[
+            title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
             Icon(IconData(0xe935, fontFamily: 'MaterialIcons')),
-          ],)
+            SizedBox(width: 10),
+            Text('My Calendar')
+          ],
+        )),
+        body: CalendarCarousel(
         ),
-        body: Column(),
       ),
-     );
-   } 
+    );
+  }
 }
